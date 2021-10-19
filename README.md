f4159de921216ac59b1e0743bdb5fb3df94b45d6
Rémi Bardon <remi.bardon.dev@gmail.com>
2021-10-19 23:36:54 +0200

    Initial Commit

---
+++ README.md
@@ -0,0 +1,3 @@
+# MonkiMapSearchParser
+
+A description of this package.
---
+++ Sources/MonkiMapSearchParser/MonkiMapSearchParser.swift
@@ -0,0 +1,6 @@
+public struct MonkiMapSearchParser {
+    public private(set) var text = "Hello, World!"
+
+    public init() {
+    }
+}
